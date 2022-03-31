19f5d272d7f6998bfe67622a5560fd63c245e558
Cosmic-rare <ysolakey1116@gmail.com>
2022-03-31 10:54:39 +0900

    Create Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { useLiveQuery } from "dexie-react-hooks"
+import Dexie from "dexie"
 
-function App() {
+const db = new Dexie("sampleDatabase")
+db.version(1).stores({
+  tasks: "++id, title, done"
+})
+
+const TaskItem = (props) => {
+  const { task } = props
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <li
+      key={task.id}
+      style={{
+        cursor: "pointer"
+      }}
+      onClick={() => {
+        db.tasks
+          .where({
+            id: task.id
+          })
+          .modify((f) => { f.done = !f.done })
+      }}
+    >
+      <span
+        onClick={() => { db.tasks.where({ id: task.id }).delete() }}
+        style={{ marginRight: 6 }}
+      >
+        {"🗑️"}
+      </span>
+      {task && task.done ? "👌" : "👋"}{" "}
+      <span
+        style={task.done ? {
+          textDecoration: "line-through"
+        } : {}}
+      >
+        {task.title}
+      </span>
+    </li>)
+
 }
 
-export default App;
+const App = () => {
+  const [title, setTitle] = useState("")
+
+  const onHandleSubmit = () => {
+    db.tasks.add({
+      title: title,
+      done: false
+    })
+    setTitle("")
+  }
+
+  const tasks = useLiveQuery(
+    () => db.tasks.toArray()
+  )
+
+  return (
+    <div>
+      <h2>ToDo</h2>
+
+      <input
+        value={title}
+        style={{
+          marginRight: 8
+        }}
+        onChange={(e) => {
+          setTitle(e.target.value)
+        }}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            onHandleSubmit()
+          }
+        }}
+      />
+
+      <button onClick={onHandleSubmit}>Create</button>
+
+      <ul style={{ listStyle: "none" }}>
+        {tasks?.map(task => <TaskItem key={task.id} task={task} />)}
+      </ul>
+
+    </div>
+  )
+}
+
+export default App
